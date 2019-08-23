2aa62abb70b3164f99f373d053288f8931c2938b
saubhik <saubhik.singh01@gmail.com>
2019-08-23 18:51:37 +0530

    Initial

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -18,6 +19,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
